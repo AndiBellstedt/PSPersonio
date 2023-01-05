a267be1a9cc4f4fb505f3f57e4a9f7572cf59a0b
Andi Bellstedt <bellstedt@gmx.de>
2023-01-05 10:46:37 +0100

    add filtering, minor bugfixes, add examples

--- PSPersonio/functions/Absence/Get-PERSAbsenceType.ps1
+++ PSPersonio/functions/Absence/Get-PERSAbsenceType.ps1
@@ -26,6 +26,21 @@
 
         Get all available absence types
 
+    .EXAMPLE
+        PS C:\> Get-PERSAbsenceType -Name "Krankheit*"
+
+        Get all available absence types with name "Krankheit*"
+
+    .EXAMPLE
+        PS C:\> Get-PERSAbsenceType -Id 10
+
+        Get absence types with id 10
+
+    .EXAMPLE
+        PS C:\> Get-PERSAbsenceType -Id 10, 11, 12 -Name "*Krankheit*"
+
+        Get absence types with id 10, 11, 12 as long, as name matches *Krankheit*
+
     .NOTES
         Author: Andreas Bellstedt
 
@@ -60,6 +75,7 @@
     end {
         if (-not $Token) { $Token = $script:PersonioToken }
 
+
         # Prepare query
         $invokeParam = @{
             "Type"    = "GET"
@@ -73,35 +89,66 @@
                     "offset" = 0
                 }
             )
+
         }
 
         # Execute query
         Write-PSFMessage -Level Verbose -Message "Getting available absence types" -Tag "AbsenseType", "Query"
         $response = Invoke-PERSRequest @invokeParam
 
+
         # Check respeonse
         if ($response.success) {
             # Check pagination / result limitation
             if ($response.metadata) {
-                Write-PSFMessage -Level Significant -Message "Pagination detected! Retrieved records: $([Array]($responseContent.data).count) of $($responseContent.metadata.total_elements) total records (api call hast limit of $($responseContent.limit) records and started on record number $($responseContent.offset))" -Tag "WebRequest", "Pagination"
+                Write-PSFMessage -Level Significant -Message "Pagination detected! Retrieved records: $([Array]($response.data).count) of $($response.metadata.total_elements) total records (api call hast limit of $($response.limit) records and started on record number $($response.offset))" -Tag "AbsenseType", "Query", "WebRequest", "Pagination"
             }
 
+
             # Process result
+            $output = [System.Collections.ArrayList]@()
             foreach ($record in $response.data) {
-                #$result = $record.attributes
-                #$result.psobject.TypeNames.Insert(0,"Personio.Absence.$($record.type)")
-                #$result.psobject.typenames.Insert(0,"Personio.Absence.AbsenceType")
+                Write-PSFMessage -Level Debug -Message "Working on record $($record.attributes.name) (ID: $($record.attributes.id))" -Tag "AbsenseType", "ObjectCreation"
 
                 # Create object
                 $result = [Personio.Absence.AbsenceType]@{
                     BaseObject = $record.attributes
-                    Id = $record.attributes.id
+                    Id         = $record.attributes.id
                 }
-                $result.psobject.TypeNames.Insert(1,"Personio.Absence.$($record.type)")
+                $result.psobject.TypeNames.Insert(1, "Personio.Absence.$($record.type)")
 
-                # Output result object
-                $result
+                # add objects to output array
+                $null = $output.Add($result)
             }
+            Write-PSFMessage -Level Verbose -Message "Retrieve $($output.Count) objects of type [Personio.Absence.AbsenceType]" -Tag "AbsenseType", "Result"
+
+
+            # Filtering
+            if ($Name -and $output) {
+                Write-PSFMessage -Level Verbose -Message "Filter by Name: $([string]::Join(", ", $Name))" -Tag "AbsenseType", "Filtering", "NameFilter"
+
+                $newOutput = [System.Collections.ArrayList]@()
+                foreach ($item in $output) {
+                    foreach ($filter in $Name) {
+                        $filterResult = $item | Where-Object Name -like $filter
+                        if($filterResult) { $null = $newOutput.Add($filterResult) }
+                    }
+                }
+
+                $Output = $newOutput
+                Remove-Variable -Name newOutput, filter, filterResult, item -Force -WhatIf:$false -Confirm:$false -Verbose:$false -Debug:$false -ErrorAction Ignore -WarningAction Ignore -InformationAction Ignore
+            }
+
+            if ($Id -and $output) {
+                Write-PSFMessage -Level Verbose -Message "Filter by Id: $([string]::Join(", ", $Id))" -Tag "AbsenseType", "Filtering", "IdFilter"
+                $output = $output | Where-Object Id -in $Id
+            }
+
+
+            # output final results
+            Write-PSFMessage -Level Verbose -Message "Output $($output.Count) objects" -Tag "AbsenseType", "Result", "Output"
+            $output
+
         } else {
             Write-PSFMessage -Level Warning -Message "Personio api reported no data" -Tag "AbsenseType", "Query"
         }
