d298f39792a95c8d60df18ae270eb0a2d62d2aca
Andi Bellstedt <bellstedt@gmx.de>
2023-01-08 08:49:39 +0100

    add class as preparation for cache machanics

---
+++ library/PSPersonio/PSPersonio/Core/Personio.Core.Cache.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Personio.Core {
+    /// <summary>
+    /// Hashtables for cache data to exchange content between runspaces
+    /// </summary>
+    public static class Cache {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static System.Collections.Specialized.OrderedDictionary Data = new System.Collections.Specialized.OrderedDictionary();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static bool StopValidationRunspace = new bool();
+    }
+}
